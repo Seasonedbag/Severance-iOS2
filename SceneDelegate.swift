2f70c5d71c5ba35ddf87d9f869b367839b8ea7c2
Seasonedbag <tonkevin18@gmail.com>
2025-11-15 08:37:18 -0500

    Add files via upload

--- SceneDelegate.swift
+++ SceneDelegate.swift
@@ -1,14 +1,2 @@
 import UIKit
-
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-    var window: UIWindow?
-    func scene(_ scene: UIScene,
-               willConnectTo session: UISceneSession,
-               options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = (scene as? UIWindowScene) else { return }
-        let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = ViewController()
-        self.window = window
-        window.makeKeyAndVisible()
-    }
-}
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {}
